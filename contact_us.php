7341cfc41d5bb5a0ae2240f02c4187f26bd683f6
Parmeet Singh Sandhu <102623572+parmeetsingh1313@users.noreply.github.com>
2022-07-20 19:15:56 +0530

    Add files via upload
    Download the zip file
    Download VS Code
    Download Xampp Server
    Run the Project on localhost

---
+++ contact_us.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
+
+	    <title>Terms and Conditions</title>
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+		<link rel="stylesheet" href="assets/css/config.css">
+		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" href="assets/images/favicon.png">
+	</head>
+    <body class="cnt-home">
+	
+<header class="header-style-1">
+
+	<!-- ============================================== TOP MENU ============================================== -->
+<?php include('includes/top-header.php');?>
+<!-- ============================================== TOP MENU : END ============================================== -->
+<?php include('includes/main-header.php');?>
+	<!-- ============================================== NAVBAR ============================================== -->
+<?php include('includes/menu-bar.php');?>
+<!-- ============================================== NAVBAR : END ============================================== -->
+
+</header>
+<!-- ============================================== HEADER : END ============================================== -->
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="index.php">Home</a></li>
+				<li class='active'>Contact Us</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content outer-top-bd">
+	<div class="container">
+		<div class="track-order-page inner-bottom-sm">
+			<div class="row">
+				<div class="col-md-12">
+	<br>
+		<p>To reach our customer service team please email us at:<a href="#">customerservice@electro.com</a></p>
+		<hr></hr>
+		<font size="4.5">Office Address</font>
+		<br><br>
+		<b>Mumbai Educational Trust - MET League of Colleges,</b>
+		<p>Bhujbal Knowledge City, Reclamation, Bandra West,Mumbai, Maharashtra 400050</p>
+		<br>
+		<p>Contact No: +91 75063 07029, +91 83691 48449</p>
+	   <div class="clear"></div>
+</div>			
+</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+<div 
+
+<?php echo include('includes/brands-slider.php');?>
+</div>
+</div>
+<?php include('includes/footer.php');?>
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	
+	<!-- For demo purposes – can be removed on production : End -->
+
+	
+
+</body>
+</html>
---
+++ terms_conditions.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
+
+	    <title>Terms and Conditions</title>
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+		<link rel="stylesheet" href="assets/css/config.css">
+		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" href="assets/images/favicon.png">
+	</head>
+    <body class="cnt-home">
+	
+<header class="header-style-1">
+
+	<!-- ============================================== TOP MENU ============================================== -->
+<?php include('includes/top-header.php');?>
+<!-- ============================================== TOP MENU : END ============================================== -->
+<?php include('includes/main-header.php');?>
+	<!-- ============================================== NAVBAR ============================================== -->
+<?php include('includes/menu-bar.php');?>
+<!-- ============================================== NAVBAR : END ============================================== -->
+
+</header>
+<!-- ============================================== HEADER : END ============================================== -->
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="index.php">Home</a></li>
+				<li class='active'>Terms And Conditions</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content outer-top-bd">
+	<div class="container">
+		<div class="track-order-page inner-bottom-sm">
+			<div class="row">
+				<div class="col-md-12">
+	<font size="4.5">Personal Information:</font>
+		<br><br>
+		<p align="justify">Products and services furnished by Seller are sold only on the terms and conditions stated herein, notwithstanding any different or additional terms or conditions stated on Customer´s purchase order or other documents, which are expressly rejected hereby by Seller.
+		Seller´s performance of any contract is expressly made conditional on Customer´s agreement to the terms and conditions stated herein unless otherwise specifically agreed to in writing by Seller.  In the absence of such agreement, commencement of performance and/or delivery shall be deemed to be for Customer´s convenience only and shall not be deemed or construed to be acceptance of Customer´s terms and conditions or any of them.
+		Further, customer´s acceptance of any product or service shall be deemed acceptance of the terms and conditions stated herein.
+		The sale of products delivered by Seller in North America, Asia and Europe shall be governed by the laws of the State of Delaware (U.S.A.), Singapore and England, respectively, notwithstanding any conflict of laws principles.
+		The United Nations Conventions on Contracts for the International Sale of Goods shall not apply.</p>
+		<hr></hr>
+		<font size="4.5">Services overview:</font>
+		<br><br>
+		<p align="justify">All prices are subject to the terms and conditions stated herein as well as any additional terms and conditions that may appear on the face hereof. 
+		In the case of a conflict between the terms and conditions stated herein and those appearing on the face hereof, the latter shall control.
+		Seller´s prices are subject to change without notice. 
+		Unless otherwise stated in writing by Seller, all prices shall be exclusive of transportation, insurance, taxes (including, without limitation, any sales, use, value added, excise or similar tax, and any tax levied on or assessed to Seller after product delivery by reason of Seller´s retention of title as provided herein) license fees, customs fees, duties, premiums, fees and other charges. 
+		Any such taxes, fees and charges will at Seller´s option be added to the price, paid directly by the Customer or reimbursed by customer if paid by Seller. Prices are for products only and do not include equipment, tools, dies, technical data, proprietary rights of any kind, patent rights, qualification tests, environmental tests or other tests (except Seller´s standard tests) or packaging (other than Seller´s standard packaging), unless expressly agreed to in writing by Seller.
+		Prices are for a specified print and revision, or if not so stated, the applicable Seller´s part number and current revision shall govern. 
+		Any changes in specified customer print shall require requotation for price and delivery.</p>
+		<hr></hr>
+		<font size="4.5">Eligibility:</font>
+		<br><br>
+		<p align="justify">Services of the Site would be available to only select geographies in India.
+		Persons who are "incompetent to contract" within the meaning of the Indian Contract Act, 1872 including un-discharged insolvents etc. are not eligible to use the Site.
+		If you are a minor i.e. under the age of 18 years but at least 13 years of age you may use the Site only under the supervision of a parent or legal guardian who agrees to be bound by these Terms of Use. 
+		If your age is below 18 years your parents or legal guardians can transact on behalf of you if they are registered users. 
+		You are prohibited from purchasing any material which is for adult consumption and the sale of which to minors is prohibited</p>
+	   <div class="clear"></div>
+</div>			
+</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+<div 
+
+<?php echo include('includes/brands-slider.php');?>
+</div>
+</div>
+<?php include('includes/footer.php');?>
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	
+	<!-- For demo purposes – can be removed on production : End -->
+
+	
+
+</body>
+</html>
